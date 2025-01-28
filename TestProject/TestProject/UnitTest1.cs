9e6b6fd71b66508a3fcfde2b2c55a80a026d70c3
Zuleyka Urieta <110435996+zulyu@users.noreply.github.com>
2025-01-27 21:31:37 -0800

    Update UnitTest1.cs

--- TestProject/TestProject/UnitTest1.cs
+++ TestProject/TestProject/UnitTest1.cs
@@ -11,6 +11,8 @@ public class UnitTest1
     [Fact]
     public void FalseIsFalse()
     {
-        Assert.False(!true);
+        // This was stupid
+        Assert.False(false);
+        Assert.True(false);
     }
 }
